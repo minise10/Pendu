4fb5f619c59901658c5d4e84db2a5f45bddeac69
minise10 <jlmodeste@hotmail.fr>
2019-01-25 10:41:08 +0100

    ajout d'index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="./KNACSS/css/knacss.css"/>
+	<link rel="stylesheet" href="./KNACSS/css/grillade-flex.css"/>
+	<link rel="stylesheet" href="./css/style.css"/>
+    <title>Trombino</title>
+  </head>
+  <body>
+      
+      <p>test</p>
+
+    <!-- jQuery -->
+    <script
+          src="https://code.jquery.com/jquery-3.3.1.min.js"
+          integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+          crossorigin="anonymous">
+    </script>
+  </body>
+</html>
